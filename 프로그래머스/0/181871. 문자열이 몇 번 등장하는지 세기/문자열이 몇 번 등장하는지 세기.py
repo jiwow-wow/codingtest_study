f3bf089734286878bbaeec2a67f5713f359cf963
jiwow-wow <jibbang@mju.ac.kr>
2026-06-24 01:21:22 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.08 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -0,0 +1,10 @@
+def solution(myString, pat):
+    answer = 0
+    
+    pat_len = len(pat)
+    
+    for i in range(len(myString) - pat_len + 1):
+        if myString[i:i+pat_len] == pat:
+            answer += 1
+            
+    return answer
