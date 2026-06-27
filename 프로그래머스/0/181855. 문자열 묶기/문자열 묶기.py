d1e25bc754029ed61100c80095b0c18cc6c2a3eb
jiwow-wow <jibbang@mju.ac.kr>
2026-06-27 21:01:50 +0900

    [level 0] Title: 문자열 묶기, Time: 17.17 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -9,3 +9,15 @@ def solution(strArr):
         arr.append(answer.count(i))
         
     return max(arr)
+
+
+'''
+딕셔너리 사용
+def solution(strArr):
+    d = {}
+
+    for i in strArr:
+        d[len(i)] = d.get(len(i), 0) + 1
+
+    return max(d.values())
+'''
