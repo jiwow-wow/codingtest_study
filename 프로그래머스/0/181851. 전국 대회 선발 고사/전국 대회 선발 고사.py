8a6d42fe0e3e1998a3d15c276520b998a18ab1c5
jiwow-wow <jibbang@mju.ac.kr>
2026-06-28 21:02:01 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -9,3 +9,11 @@ def solution(rank, attendance):
     arr = list(soted_dic.keys())
     
     return arr[0] * 10000 + arr[1]*100 + arr[2]
+
+'''
+리스트컴프리핸션까지는 생각했으나, set을 써서 넣는 방법을 생각하지 못함, 2중 리스트로 시도하다가 실패하였음
+
+def solution(rank, attendance):
+    arr = sorted([(x, i) for i, x in enumerate(rank) if attendance[i]])
+    return arr[0][1] * 10000 + arr[1][1] * 100 + arr[2][1]
+'''
