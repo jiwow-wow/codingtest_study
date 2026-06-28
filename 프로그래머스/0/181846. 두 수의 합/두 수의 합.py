047865da0e1303305baef63dd7813ebc0aa3a45e
jiwow-wow <jibbang@mju.ac.kr>
2026-06-28 21:23:22 +0900

    [level 0] Title: 두 수의 합, Time: 29.47 ms, Memory: 17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.py
@@ -0,0 +1,23 @@
+# def solution(a, b):
+#     return str(int(a) + int(b))
+
+
+def solution(a, b):
+    result = []
+    carry = 0
+
+    i = len(a) - 1
+    j = len(b) - 1
+
+    while i >= 0 or j >= 0 or carry:
+        num1 = int(a[i]) if i >= 0 else 0
+        num2 = int(b[j]) if j >= 0 else 0
+
+        total = num1 + num2 + carry
+        result.append(str(total % 10))
+        carry = total // 10
+
+        i -= 1
+        j -= 1
+
+    return ''.join(result[::-1])
