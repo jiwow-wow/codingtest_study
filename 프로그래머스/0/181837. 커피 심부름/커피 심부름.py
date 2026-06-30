0f02fa7d9fe4d346fc0d9217ec1c0601f2249bac
jiwow-wow <jibbang@mju.ac.kr>
2026-06-30 21:27:43 +0900

    [level 0] Title: 커피 심부름, Time: 0.06 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+    
+    for o in order:
+        if ('americano' in o) or o=='anything':
+            answer += 4500
+        elif 'cafelatte' in o:
+            answer += 5000
+            
+    return answer
