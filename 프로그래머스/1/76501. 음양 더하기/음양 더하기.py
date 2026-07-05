7bb03f190207b352cb307eb18bff51f358f61ada
Jiwon Jung <jibbang@mju.ac.kr>
2026-07-05 11:47:52 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,3 +1,8 @@
 def solution(absolutes, signs):
     answer =[ a if (signs[i]==1) else -a for i,a in enumerate(absolutes)  ]
     return sum(answer)
+
+"""
+zip함수 사용하기
+sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
+"""
