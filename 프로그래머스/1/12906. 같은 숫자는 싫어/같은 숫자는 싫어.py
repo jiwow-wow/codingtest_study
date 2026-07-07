1ad342e43ec0b515ac168b002c511a03aa23968c
Jiwon Jung <jibbang@mju.ac.kr>
2026-07-07 17:32:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 67.39 ms, Memory: 26.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,21 @@
+def solution(arr):
+    answer = []
+    
+    
+    for i in range(len(arr)):
+        if i ==0 or arr[i] != answer[-1] :
+            answer.append(arr[i])    
+    return answer 
+
+
+# def solution(arr):
+#     answer = []
+    
+    
+#     for i in range(len(arr)):
+#         if i ==0:
+#             answer.append(arr[i])
+#         if arr[i] != answer[-1]:
+#             answer.append(arr[i])
+    
+#     return answer 
