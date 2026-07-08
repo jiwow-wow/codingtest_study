ba05edb8236e05e053248efd741f95ceaa1580fa
Jiwon Jung <jibbang@mju.ac.kr>
2026-07-08 21:32:54 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -10,3 +10,14 @@ def solution(number):
     
     
     return answer
+
+"""
+라이브러리 사용
+def solution (number) :
+    from itertools import combinations
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
+    return cnt
+"""
