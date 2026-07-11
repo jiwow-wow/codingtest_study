54ab440fd42f19c41216fe539cac7c695a93e369
Jiwon Jung <jibbang@mju.ac.kr>
2026-07-11 20:00:00 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -4,3 +4,8 @@ def solution(array, commands):
         sorted_arr = sorted(array[ c[0]-1: c[1]  ])
         answer.append(sorted_arr[c[2] -1])
     return answer
+
+"""
+return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+
+"""
