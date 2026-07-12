cd00fbb062a4793b552a076d895c5a65b641f0e9
Jiwon Jung <jibbang@mju.ac.kr>
2026-07-12 14:41:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -2,7 +2,19 @@ def solution(s):
     answer = 0
     string_list = ["zero", "one", "two", "three", 'four', 'five', 'six', 'seven', 'eight', 'nine']
     
-    # if string_list in s:
     for st in string_list:
         s = s.replace(st, str(string_list.index(st)))
     return int(s)
+
+
+"""
+딕셔너리를 활용한 풀이:
+
+num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+
+def solution(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+    return int(answer)
+"""
